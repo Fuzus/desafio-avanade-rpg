3bda4b55d7b2d78a0093c682c8f91ad19369d560
Fuzus <matheusdamata2000@gmail.com>
2023-09-12 11:52:04 -0300

    first commit

---
+++ src/main/java/br/com/fuzus/avanadedesafiorpg/AvanadeDesafioRpgApplication.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.avanadedesafiorpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDesafioRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDesafioRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fuzus/avanadedesafiorpg/AvanadeDesafioRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuzus.avanadedesafiorpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeDesafioRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
