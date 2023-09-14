cc80d7be18477a22281ab646a9b6b1eb8c7f9044
Fuzus <matheusdamata2000@gmail.com>
2023-09-14 12:52:35 -0300

    Corrigindo logica do diceRoll

--- src/main/java/br/com/fuzus/avanadedesafiorpg/domain/battle/service/actions/dice/DiceRoll.java
+++ src/main/java/br/com/fuzus/avanadedesafiorpg/domain/battle/service/actions/dice/DiceRoll.java
@@ -8,7 +8,7 @@ public class DiceRoll {
 
     public static int diceRoll(Dice dice){
         Random random = new Random();
-        return random.nextInt(1, dice.faces);
+        return random.nextInt(1, dice.faces + 1);
     }
 
 }
